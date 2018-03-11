c3d1b0ac181892df19394d55396ad4650546a223
astelvida <sevda.m.anefi@gmail.com>
2018-03-11 13:51:26 +0000

    create repo and terminal layout

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+<div id="app">
+    <div class="main flex-col">
+        <div class="toolbar flex-row">
+            <div class="osx-icons flex-row">
+                <img class="osx-icon" src="./assets/osx_red_icon.png">
+                <img class="osx-icon" src="./assets/osx_orange_icon.png">
+                <img class="osx-icon" src="./assets/osx_green_icon.png">
+            </div>
+        </div>
+
+        <div class="screen flex-col">
+            
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+        
+        }
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+
+    },
+}
+</script>
+
+<style lang="scss">
+@import './assets/normalize';
+@import './assets/app-style.scss';
+
+</style>
